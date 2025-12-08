24505df75c45f4a457f602f93d746dccd1480189
cuna-max <crypto.synuns@gmail.com>
2025-12-09 01:47:55 +0900

    feat: add deepmerge-ts for configuration merging
    - Added deepmerge-ts as a dependency in the UI package for improved configuration merging.
    - Updated the withUI function to utilize deepmerge-ts and include global Tailwind CSS configuration presets.

--- packages/ui/lib/with-ui.ts
+++ packages/ui/lib/with-ui.ts
@@ -1,7 +1,9 @@
-import deepmerge from 'deepmerge';
+import globalConfig from '@extension/tailwindcss-config';
+import { deepmerge } from 'deepmerge-ts';
 import type { Config } from 'tailwindcss';
 
 export const withUI = (tailwindConfig: Config): Config =>
   deepmerge(tailwindConfig, {
     content: ['../../packages/ui/lib/**/*.tsx'],
+    presets: [globalConfig],
   });
